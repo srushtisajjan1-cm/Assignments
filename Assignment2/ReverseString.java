c0d5fdbf3a84a5dda5ab31b3300a53e17ad7fadc
srushtisajjan1-cm <srushtisajjan1@gmail.com>
2026-03-11 19:44:43 +0530

    Delete Assignment2 directory

--- Assignment2/ReverseString.java
+++
@@ -1,8 +0,0 @@
-public class ReverseString {
-    public static void main(String[] args) {
-        String str = "Java";
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        System.out.println("Reversed String: " + sb);
-    }
-}
